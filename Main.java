238b67e61ab30dec4adadcb1c7afdf2858cfe586
JOICE THAYNARA <105690477+JoiceBarbosa1@users.noreply.github.com>
2022-07-12 20:41:47 +0000

    Segundo commit

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println(" Olá usuário!");
+    System.out.println("Tudo bem?");
+
   }
 }
